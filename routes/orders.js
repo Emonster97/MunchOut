9b994565df00eb0b9e777ab8fc70380ba52405fb
Wilfred <wilfredemonts@gmail.com>
2022-01-14 03:53:49 -0500

    add and install twilio package, add alert to stop user from placing an empty, add get to let user know when order is ready with the fetch calling it and the timeout on the front end ejs file, rework get id queries for consistency with codebase, add and change query to select the order and its details and send them through templateVars to the frontend, add query and twilio api to send order information to the specified number (restaurant)  via sms, add all order information, calculate totals, and order times and display to users on orders page, add ability to vew old orders, add button to place a new order

--- routes/orders.js
+++ routes/orders.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router  = express.Router();
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+const twilioNumber = process.env.TWILIO_PHONE_NUMBER;
 
 module.exports = (db) => {
   router.get('/', function (req, res){
@@ -12,17 +17,32 @@ module.exports = (db) => {
 
 });
 
+router.get("/order_ready", () => {
+  client.messages
+  .create({
+     body: 'Your order is ready for pickup!',
+     from: twilioNumber,
+     to: '16134004750'
+   })
+  .then(message => console.log(message.sid));
+});
+//   db.query(`SELECT items.name, order_items.quantity FROM items JOIN order_items ON order_items.order_id=${req.cookies["order"]} AND order_items.item_id=items.id`)
 router.get('/:id', function (req, res){
-  db.query("SELECT * FROM orders WHERE user_id = $1 AND id = $2", [req.cookies.id, req.params.id]).then(async data => {
-    const itemsResult = await db.query("SELECT * FROM order_items JOIN items ON order_items.item_id = items.id WHERE order_id = $1", [req.params.id])
-    const items = itemsResult.rows
-    console.log(items)
-    const order = data.rows[0]
-    console.log(order);
-    const templateVars = {order : order, items : items}
-    res.render('order', templateVars)
-  });
-
+  db.query("SELECT * FROM orders WHERE user_id = $1 AND id = $2", [req.cookies.id, req.params.id]).then(data => {
+    const order = data.rows[0];
+    db.query("SELECT * FROM order_items JOIN items ON order_items.item_id = items.id WHERE order_id = $1", [req.params.id]).then(itemsResult => {
+      const items = itemsResult.rows;
+      db.query(`SELECT * FROM order_items WHERE order_items.order_id=${order.id}`).then(orderItem => {
+        const order_items = orderItem.rows;
+        const templateVars = {
+          order : order,
+          items : items,
+          order_items: order_items
+        };
+        res.render('order', templateVars);
+      }).catch(err => console.log(err));
+    }).catch(err => console.log(err));
+  }).catch(err => console.log(err));
 });
 
 router.post('/', function (req, res){
@@ -30,7 +50,6 @@ router.post('/', function (req, res){
   let orderId = "";
   // get all data about the order and save it in db, send SMS to user, send SMS to restaurant, res.redirect("/order);
     // set timeout send sms when  time expires to user and update db with status - ready for pick up
-
     //make array of items using req body use multiple insert queries through array to dump them into order items under the correct order id
 
     //if (res.cookie["order"]) { get outa here }
@@ -41,29 +60,40 @@ router.post('/', function (req, res){
   db.query(`INSERT INTO orders (user_id, status, place_order_time) VALUES ($1, 'Placed Order', $2) RETURNING *`,[Number(req.cookies['id']),Date.now()])
   .then(data => {
     const order = data.rows[0];
+
     orderId = order.id;
 
-
-
     let values = [];
+
     for (let item of items) {
       values.push([Number(orderId), Number(item.id), Number(item.quantity)])
     }
 
     const insertSQL = "INSERT INTO order_items (order_id, item_id, quantity) VALUES %L RETURNING *"
+
     let format = require("pg-format");
+
     console.log (format(insertSQL, values));
+
     db.query(format(insertSQL, values),[]).then(data => {
-      //store order ID somewhere on user so you're able to query database to see the order
-      //res.cookie["order"] = orderId;
-      //show a different screen if order cookie is already set, so user can't place multiple orders
-      //redirect to /orders
-      res.redirect("/api/orders/"+orderId);
+      db.query(`SELECT items.name FROM items JOIN order_items ON order_items.item_id=items.id AND order_items.order_id=${orderId}`).then(data => {
+        let items = data.rows;
+        let orderMessage = "New Order: ";
+        console.log(values, items);
+        for (let i = 0; i < items.length; i++) {
+          orderMessage += values[i][2] + "x " + items[i].name + ". ";
+        }
+        console.log(orderMessage);
+        client.messages
+        .create({
+          body: orderMessage,
+          from: twilioNumber,
+          to: '16134004750'
+        })
+        .then(message => console.log(message.sid));
+        res.redirect("/api/orders/"+orderId);
+      }).catch(err => console.log(err));
     }).catch(err => console.log(err));
-
-    //SELECT * FROM order_items JOIN orders ON order_items.order_id = orders.id;
-
-    //delete res and Insert into order items, the amount of items we ordered, so if someone ordered one hot dog and one hamburger we would do 2 insert queries, we do a for loop for this
   })
   .catch(err => {
     console.log(err);
